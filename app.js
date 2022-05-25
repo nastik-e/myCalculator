4dcf9a908c46274be0f156bac8804f2fc9226340
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-05-25 15:28:25 +0300

    no % no reset after = no result bug

--- app.js
+++ app.js
@@ -2,16 +2,75 @@ const mathLine = document.getElementById ("calculator_panel_mathline");
 const result = document.getElementById ("calculator_panel_result");
 const calcButtcons = document.getElementById ("calculator_buttons");
 
-calcButtcons.addEventListener("click", function(e) {
-const button = e.target;
-const buttonValue = button.textContent;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
-// if (buttonValue === "C") {
-//     alert ("C");
-// } else {
-//     alert ("not C");
-// }
 
-mathLine.textContent = buttonValue;
+calcButtcons.addEventListener ("click", function(e)  {
+    const button = e.target;
+    const buttonValue = button.textContent;
+
+    if (buttonValue === "C") {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        mathLine.textContent = "";
+        result.textContent = "";
+       
+    } else if (button.classList.contains("number")) {
+        if (operator.length > 0) {
+            secondNumber = secondNumber + buttonValue;
+        } else {
+    firstNumber = firstNumber + buttonValue
+    };
+
+    } else if (button.classList.contains("operator")) {
+    if (firstNumber.length === 0 || secondNumber.length !== 0) {
+            return;
+        }
+        operator = buttonValue;
+
+        //  точка
+    } else if (buttonValue === ".") {
+        let number
+            if (operator.length > 0) {
+                number = secondNumber;
+            } else {
+                number = firstNumber;
+            }
+
+        if (number.length = 0 || number.includes(".")) {
+            return;
+        }
+        if (operator.length > 0) {
+            secondNumber = secondNumber + buttonValue;
+        } else {
+    firstNumber = firstNumber + buttonValue
+        }
+
+    } else if (buttonValue === "=") {
+        if (secondNumber.length === 0) {
+            return; 
+        }
+
+        firstNumber = +firstNumber;
+        secondNumber = +secondNumber;
+        if (operator === "/") {
+            result.textContent = firstNumber / secondNumber;
+        }
+        if (operator === "*") {
+            result.textContent = firstNumber * secondNumber;
+        }
+        if (operator === "-") {
+            result.textContent = firstNumber - secondNumber;
+        }
+        if (operator === "+") {
+            result.textContent = firstNumber + secondNumber;
+        }
+    
+    }
+
+    mathLine.textContent = `${firstNumber} ${operator} ${secondNumber}`;
 
 })
