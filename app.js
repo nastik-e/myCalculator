3d7bd850f121f21d13732b5bbce2e7eadb462b93
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-05-27 22:57:16 +0300

    all fixed

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
-function resetVar() {
-    firstNumber = "";
-     secondNumber = "";
-     operator = ""; 
-}
+
 
 
 calcButtcons.addEventListener ("click", function(e)  {
@@ -19,24 +15,34 @@ calcButtcons.addEventListener ("click", function(e)  {
 
     if (buttonValue === "C") {
             resetVar() 
-        mathLine.textContent = "";
         result.textContent = "";
     
-    
-    } else if (button.classList.contains("number")) {
+   } else if (button.classList.contains("delete")) {
+        if (secondNumber.length > 0) {
+        secondNumber = secondNumber.slice(0, secondNumber.length - 1);
+        } else if (operator.length > 0) {
+        operator = "";
+        } else {
+        firstNumber = firstNumber.slice(0, secondNumber.length - 1);
+        }    
+    showMathLine()
+  
+    }  else if (button.classList.contains("number")) {
         if (operator.length > 0) {
             secondNumber = secondNumber + buttonValue;
+            showMathLine()
         } else {
     firstNumber = firstNumber + buttonValue
-    };
+    showMathLine()
+    }
 
     } else if (button.classList.contains("operator")) {
     if (firstNumber.length === 0 || secondNumber.length !== 0) {
             return;
         }
         operator = buttonValue;
+        showMathLine()
 
-        //  точка
     } else if (buttonValue === ".") {
         let number
             if (operator.length > 0) {
@@ -52,6 +58,7 @@ calcButtcons.addEventListener ("click", function(e)  {
             secondNumber = secondNumber + buttonValue;
         } else {
     firstNumber = firstNumber + buttonValue
+    showMathLine()
         }
 
 
@@ -75,20 +82,19 @@ calcButtcons.addEventListener ("click", function(e)  {
             result.textContent = firstNumber / 100 * secondNumber;
         }
 
-        resetVar()
+     resetVar()
+        }
+        
+    function resetVar() {
+        firstNumber = "";
+         secondNumber = "";
+         operator = ""; 
+         mathLine.textContent = "";
     }
 
-    mathLine.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-// Хуита какая-то с этой кнопкой
-        if (button.classList.contains("delete")) {
-                                        console.log('до: ' + mathLine.textContent)
-                                        console.log ('length ' + mathLine.textContent.length);
-            //let lastNumber = mathLine.textContent [mathLine.textContent.length - 3];
-            //                            console.log ('last ' + lastNumber);
-            let newLine = mathLine.textContent.slice (0, mathLine.textContent.length - 3);
-                                        console.log ('обрезанная ' + newLine);
-            mathLine.textContent = newLine;
-            console.log('new mathLine= ' + mathLine.textContent);
-
-        }
+    function showMathLine() {
+        mathLine.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    }
+    
 })
+
