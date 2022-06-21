851d976115ff5c3d1135588cb7c8e229ad471575
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-22 01:03:29 +0300

    fix: the look of js

--- app.js
+++ app.js
@@ -1,106 +1,99 @@
-const mathLine = document.getElementById ("calculator_panel_mathline");
-const result = document.getElementById ("calculator_panel_result");
-const calcButtcons = document.getElementById ("calculator_buttons");
+const mathLine = document.getElementById("calculator_panel_mathline");
+const result = document.getElementById("calculator_panel_result");
+const calcButtcons = document.getElementById("calculator_buttons");
 
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
-calcButtcons.addEventListener ("click", function(e)  {
-    const button = e.target;
-    const buttonValue = button.textContent;
+calcButtcons.addEventListener("click", function (e) {
+  const button = e.target;
+  const buttonValue = button.textContent;
 
-        if (buttonValue === "C" || buttonValue === "AC") {
-            resetVar() 
-        result.textContent = "";
-    
-   } else if (button.classList.contains("delete")) {
-        if (secondNumber.length > 0) {
-        secondNumber = secondNumber.slice(0, secondNumber.length - 1);
-        } else if (operator.length > 0) {
-        operator = "";
-        } else {
-        firstNumber = firstNumber.slice(0, secondNumber.length - 1);
-        }    
-    showMathLine()
-  
-    }  else if (button.classList.contains("number")) {
-        if (operator.length > 0) {
-            secondNumber = secondNumber + buttonValue;
-            showMathLine()
-        } else {
-    firstNumber = firstNumber + buttonValue
-    showMathLine()
-    }
-
-    } else if (button.classList.contains("operator")) {
-    if (firstNumber.length === 0 || secondNumber.length !== 0) {
-            return;
-        }
-        operator = buttonValue;
-        showMathLine()
-
-    } else if (buttonValue === ".") {
-        let number
-            if (operator.length > 0) {
-                number = secondNumber;
-            } else {
-                number = firstNumber;
-            }
-
-        if (number.length = 0 || number.includes(".")) {
-            return;
-        }
-        if (operator.length > 0) {
-            secondNumber = secondNumber + buttonValue;
-        } else {
-    firstNumber = firstNumber + buttonValue
-    showMathLine()
-        }
-
-
-    } else if (buttonValue === "=") {
-        if (secondNumber.length === 0) {
-            return; 
-        }
-
-        firstNumber = +firstNumber;
-        secondNumber = +secondNumber;
- 
-        if (operator === "/") {
-            result.textContent = firstNumber / secondNumber;
-        } else if (operator === "*") {
-            result.textContent = firstNumber * secondNumber;
-        } else if (operator === "-") {
-            result.textContent = firstNumber - secondNumber;
-        } else if (operator === "+") {
-            result.textContent = firstNumber + secondNumber;
-        } else if (operator === "%") {
-            result.textContent = firstNumber / 100 * secondNumber;
-        }
-
-     resetVar()
-        }
-        
-    function resetVar() {
-        firstNumber = "";
-         secondNumber = "";
-         operator = ""; 
-         mathLine.textContent = "";
-    }
-
-    function showMathLine() {
-        mathLine.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-    }
-   
-    if (mathLine.textContent.length > 3) {
-        c1.classList.add("hidden");
-        c2.classList.remove ("hidden");
-        c3.classList.remove ("hidden");
+  if (buttonValue === "C" || buttonValue === "AC") {
+    resetVar();
+    result.textContent = "";
+  } else if (button.classList.contains("delete")) {
+    if (secondNumber.length > 0) {
+      secondNumber = secondNumber.slice(0, secondNumber.length - 1);
+    } else if (operator.length > 0) {
+      operator = "";
     } else {
-        c1.classList.toggle("hidden")
-        c2.classList.toggle("hidden")
-        c3.classList.toggle("hidden")
+      firstNumber = firstNumber.slice(0, secondNumber.length - 1);
     }
-})
+    showMathLine();
+  } else if (button.classList.contains("number")) {
+    if (operator.length > 0) {
+      secondNumber = secondNumber + buttonValue;
+      showMathLine();
+    } else {
+      firstNumber = firstNumber + buttonValue;
+      showMathLine();
+    }
+  } else if (button.classList.contains("operator")) {
+    if (firstNumber.length === 0 || secondNumber.length !== 0) {
+      return;
+    }
+    operator = buttonValue;
+    showMathLine();
+  } else if (buttonValue === ".") {
+    let number;
+    if (operator.length > 0) {
+      number = secondNumber;
+    } else {
+      number = firstNumber;
+    }
 
+    if ((number.length = 0 || number.includes("."))) {
+      return;
+    }
+    if (operator.length > 0) {
+      secondNumber = secondNumber + buttonValue;
+    } else {
+      firstNumber = firstNumber + buttonValue;
+      showMathLine();
+    }
+  } else if (buttonValue === "=") {
+    if (secondNumber.length === 0) {
+      return;
+    }
+
+    firstNumber = +firstNumber;
+    secondNumber = +secondNumber;
+
+    if (operator === "/") {
+      result.textContent = firstNumber / secondNumber;
+    } else if (operator === "*") {
+      result.textContent = firstNumber * secondNumber;
+    } else if (operator === "-") {
+      result.textContent = firstNumber - secondNumber;
+    } else if (operator === "+") {
+      result.textContent = firstNumber + secondNumber;
+    } else if (operator === "%") {
+      result.textContent = (firstNumber / 100) * secondNumber;
+    }
+
+    resetVar();
+  }
+
+  function resetVar() {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    mathLine.textContent = "";
+  }
+
+  function showMathLine() {
+    mathLine.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+  }
+
+  if (mathLine.textContent.length > 3) {
+    c1.classList.add("hidden");
+    c2.classList.remove("hidden");
+    c3.classList.remove("hidden");
+  } else {
+    c1.classList.toggle("hidden");
+    c2.classList.toggle("hidden");
+    c3.classList.toggle("hidden");
+  }
+});
